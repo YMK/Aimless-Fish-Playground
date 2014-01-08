09aa8af782e7af04d9484b529ce648780e451854
Alistair McKinlay <github@10people.co.uk>
2014-01-08 11:11:28 +0000

    Few visual fixes and enhancements
    Fixed the annoying FF scrollbar bug (showing a scrollbar when hovering
    over the sudoku board).
    
    Fixed the other annoying FF/safari bug where the background of the cell
    overlapped the border of the tr/col.
    
    Tried to fix the col:hover not working in FF (didn't work).
    
    And made the table slightly rounded. Looks a bit nicer that way :-)

--- app/js/fixes.js
+++ app/js/fixes.js
@@ -3,20 +3,34 @@ define([], function () {
 
   fixes.firefoxFix = function () {
 
-    if (/firefox/.test(window.navigator.userAgent.toLowerCase())) {
-      var tds = document.getElementsByTagName('td'),
-        style = '<style>'
-          + 'td { padding: 0 !important; }'
-          + 'td:hover::before, td:hover::after { background-color: transparent !important; }'
-          + '</style>',
-        index;
+    if ( /firefox/.test( window.navigator.userAgent.toLowerCase() ) ) {
 
-      for (index = 0; index < tds.length; index++) {
-        tds[index].innerHTML = '<div class="ff-fix">' + tds[index].innerHTML + '</div>';
-      }
+        var tds = document.getElementsByTagName( 'td' ),
+            ths = document.getElementsByTagName( 'th' );
 
-      document.head.insertAdjacentHTML('beforeEnd', style);
-    }
+        for( var index = 0; index < tds.length; index++ ) {
+
+            tds[index].innerHTML = '<div class="ff-fix">' + tds[index].innerHTML + '</div>';                     
+
+        };
+
+        for( var index = 0; index < ths.length; index++ ) {
+
+            ths[index].innerHTML = 
+                  '<div class="' + ths[index].className + '">' 
+                + ths[index].innerHTML 
+                + '</div>';                     
+            ths[index].className = '';
+
+        };
+
+        var style = '<style>'
+            + 'td, th { padding: 0 !important; }' 
+            + 'td:hover::before, td:hover::after { background-color: transparent !important; }'
+            + '</style>';
+        document.head.insertAdjacentHTML( 'beforeEnd', style );
+
+    };
   };
 
   return fixes;
